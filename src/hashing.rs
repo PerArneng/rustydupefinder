ff3187df4b3b5cd29817b5c350455b8d3ef93b18
Per Arneng <per.arneng@scalebit.com>
2019-01-07 21:47:02 +0100

    hashing is now working with a test added

--- src/hashing.rs
+++ src/hashing.rs
@@ -1,13 +1,12 @@
-
 use digest::Digest;
 use sha2::Sha256;
-use std::io::{self, Read};
+use std::io::{self};
 use std::fs::{File};
 use std::path::Path;
 use itertools::Itertools;
 
 
-pub fn read_hash(read:&mut Read) -> io::Result<String> {
+pub fn read_hash(read:&mut io::Read) -> io::Result<String> {
     let mut digest = Sha256::new();
     let _result = io::copy(read, &mut digest)?;
     let result = digest.result();
@@ -15,13 +14,16 @@ pub fn read_hash(read:&mut Read) -> io::Result<String> {
     return Ok(hex_string);
 }
 
+
 pub fn path_hash(path:&Path) -> io::Result<String> {
     let mut file:File = File::open(path)?;
     return read_hash(&mut file);
 }
 
+
 #[test]
-fn create_hash_test() {
-    let str = read_hash(&mut BufReader::new("Potato!".as_bytes()));
-    assert_eq!("hex {}", str.expect("a real string"));
+fn test_read_hash() {
+    let str = read_hash(&mut io::BufReader::new("Potato!".as_bytes()));
+    assert_eq!("bf82efa58491263cf5c3c8866f7f20ec18d25f2c0894b323184a7625ed529084",
+               str.expect("a real string"));
 }
